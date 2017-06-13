4157180fd5bafc39e9831bc11f61c8a626e904a1
doulighan <doulighan@gmail.com>
2017-06-13 14:10:58 -0400

    done

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function getRepositories() {
 function showCommits() {
   const commits = JSON.parse(this.responseText)
   const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
-  document.getElementById("commits").innerHTML = commitList
+  document.getElementById("commits").innerHTML = commitsList
 }
 
 function getCommits(el) {
